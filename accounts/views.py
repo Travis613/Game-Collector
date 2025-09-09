ce0ac3ece11be66f558abb967efbc38e6d7c17bc
Travis <travissmac@Traviss-MacBook-Air.local>
2025-09-08 20:02:04 -0400

    set up basic authenication to login users in

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth import  authenticate, login, logout
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render (request, "accounts/index.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "accounts/account.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "accounts/login.html", {
+                "message": "invalid credentials"
+            })
+        
+    return render (request, "accounts/login.html")
+
+def logout_view():
+    pass
