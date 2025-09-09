fca16a2a51c224ba8b49c81fc6306fc13d86598a
Travis <travissmac@Traviss-MacBook-Air.local>
2025-09-09 16:28:48 -0400

    set up a way to log users out

--- accounts/views.py
+++ accounts/views.py
@@ -24,5 +24,8 @@ def login_view(request):
         
     return render (request, "accounts/login.html")
 
-def logout_view():
-    pass
+def logout_view(request):
+    logout(request)
+    return render(request, "accounts/login.html", {
+            "message": "logged out."
+        })
